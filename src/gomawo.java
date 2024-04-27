34fd834d355b7385247219de0efdd78bad04fdc0
jeaconia <tjeaconia8@gmail.com>
2024-04-27 14:21:00 +0800

    gomawo done

--- src/gomawo.java
+++ src/gomawo.java
@@ -1,3 +1,10 @@
-public class gomawo {
-
+public class Gomawo {
+    public static void main(String[] args) {
+        System.out.println("----------------------------");
+        System.out.println("|          GOMAWO          |");
+        System.out.println("|  Thank you For Ordering  |");
+        System.out.println("|        At SHIBPAL        |");
+        System.out.println("----------------------------");
+    }
 }
+
