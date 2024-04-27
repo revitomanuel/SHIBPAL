0569de29c03c9939f7d6d674063c9a3e35b8df7d
Revito <revitomanik@gmail.com>
2024-04-27 17:03:44 +0800

    Greetings

---
+++ src/Greetings.java
@@ -0,0 +1,17 @@
+public class Greetings {
+    public static void annyeong() {
+        System.out.println("✧====================================✧");
+        System.out.println("              ₊˚⊹ 안녕 ⊹˚₊              ");
+        System.out.println("          Welcome to Shibpal           ");
+        System.out.println("       Korean Online Food Order        ");
+        System.out.println("✧====================================✧");
+    }
+
+    public static void gomawo() {
+        System.out.println("✧====================================✧");
+        System.out.println("            ₊˚⊹ 거마워 ⊹˚₊               ");
+        System.out.println("           Enjoy Your Food             ");
+        System.out.println("   Thanks for Ordering with Shibpal    ");
+        System.out.println("✧====================================✧");
+    }
+}
