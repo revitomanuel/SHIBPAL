e10d8024ab15c200527bdcd5daf91b93d59e4ab7
jeaconia <tjeaconia8@gmail.com>
2024-04-27 16:40:30 +0800

    gomawo done

--- src/Admin.java
+++ src/Admin.java
@@ -20,4 +20,35 @@ public class Admin {
 
         scanner.close();
     }
+
+    public static void menuAdmin(){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("----------------------------");
+            System.out.println("| 1. Restaurant List       |");
+            System.out.println("| 2. Add New Restaurant    |");
+            System.out.println("| 3. Delete Restaurant     |");
+            System.out.println("| 4. Back                  |");
+            System.out.println("| 0. Exit                  |");
+            System.out.println("----------------------------");
+
+            System.out.print(" Enter Choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Invalid Choice!");
+            }
+        }
+    }
 }
