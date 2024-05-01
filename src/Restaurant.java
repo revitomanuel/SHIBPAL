cf097c42383698f3776b69da848bfcc1b2eb0fbf
jeaconia <tjeaconia8@gmail.com>
2024-05-01 23:53:24 +0800

    edit resto class

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,4 +3,31 @@ public class Restaurant {
     private String name;
     private String address;
     private ArrayList<Menu> menus;
+
+    public Restaurant(String name, String address){
+        this.name = name;
+        this.address = address;
+        this.menus = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public String makeString(){
+        //single string representation
+        return name +""+ address;
+    }
+
+    public ArrayList<Menu> getMenus() {
+        return menus;
+    }
+
+    public void addMenu(Menu menu){
+        this.menus.add(menu);
+    }
 }
