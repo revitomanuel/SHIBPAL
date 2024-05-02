e664bb1651183900c7d68da32d2ac98d31e57c26
jeaconia <tjeaconia8@gmail.com>
2024-05-03 06:52:11 +0800

    edit resto class

--- src/Admin.java
+++ src/Admin.java
@@ -29,7 +29,47 @@ public class Admin {
     }
 
     public static void restaurantInit(){
+        //restaurant 1
+        Restaurant restaurant1 = new Restaurant("ChicKen", "JL Uluwatu No. 520, Badung");
+        restaurant1.addMenu(new Menu("Original Chicken", 23000));
+        restaurant1.addMenu(new Menu("Sweet-Spicy Chicken", 27000));
+        restaurant1.addMenu(new Menu("Soy Butter Chicken", 27000));
+        restaurant1.addMenu(new Menu("Gochujang Chicken", 27000));
+        restaurant1.addMenu(new Menu("Garlic Parmesan Chicken", 27000));
+        restaurant1.addMenu(new Menu("Bburingkle Chicken", 27000));
+        restaurant1.addMenu(new Menu("Can Soda", 10000));
+        restaurant1.addMenu(new Menu("Midnight Sky Soda", 17000));
+        restaurant1.addMenu(new Menu("Sunset Road Soda", 17000));
 
+        //restaurant 2
+        Restaurant restaurant2 = new Restaurant("Bulnoriya", "JL Sunset Road No. 127, Badung");
+        restaurant2.addMenu(new Menu("Local Beef Short Skirt (200g)", 75000));
+        restaurant2.addMenu(new Menu("Local Beef Sirloin (200g)", 100000));
+        restaurant2.addMenu(new Menu("Local Beef Tongue (200g)", 80000));
+        restaurant2.addMenu(new Menu("Local Beef Rib (200g)", 90000));
+        restaurant2.addMenu(new Menu("Aus Beef Short Skirt (200g)", 100000));
+        restaurant2.addMenu(new Menu("Aus Beef Sirloin (200g)", 125000));
+        restaurant2.addMenu(new Menu("Aus Beef Tongue (200g)", 105000));
+        restaurant2.addMenu(new Menu("Aus Beef Rib (200g)", 115000));
+        restaurant2.addMenu(new Menu("Iced Tea", 10000));
+
+        //restaurant 3
+        Restaurant restaurant3 = new Restaurant("BabMyeon", "JL Raya Kuta No. 553, Badung");
+        restaurant3.addMenu(new Menu("JjangMyeon", 20000));
+        restaurant3.addMenu(new Menu("JjangBab", 18000));
+        restaurant3.addMenu(new Menu("BibimMyeon", 24000));
+        restaurant3.addMenu(new Menu("BibimBab", 18000));
+        restaurant3.addMenu(new Menu("BokkeumMyeon", 20000));
+        restaurant3.addMenu(new Menu("BokkeumBab", 20000));
+        restaurant3.addMenu(new Menu("Mul NaengMyeon", 27000));
+        restaurant3.addMenu(new Menu("Ramyeon", 18000));
+        restaurant3.addMenu(new Menu("KimBab", 22000));
+        restaurant3.addMenu(new Menu("BoriCha", 15000));
+
+        //add restaurant data
+        restaurants.put("ChicKen", restaurant1);
+        restaurants.put("Bulnoriya", restaurant2);
+        restaurants.put("BabMyeon", restaurant3);
     }
     public static void menuAdmin(){
         Scanner scanner = new Scanner(System.in);
